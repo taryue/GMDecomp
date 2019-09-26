79c30ded1edac7458a22a54fcf54ce48cf3fd772
Yue Wang <taryue@live.unc.edu>
2019-09-26 15:14:18 -0700

    Add files via upload

---
+++ R/tobacco_clr.R
@@ -0,0 +1,19 @@
+#' A smokeless tobacco data set, with 45 samples and 271 OTUs from Satten et. al (2017).
+#'
+#'  @docType data
+#'
+#'  @usage data(tobacco_clr)
+#'  @format An object including:
+#'  \itemize{
+#'
+#'  \item{\code{data}}{the CLR-transformed data matrix.}
+#'  \item{\code{H}}{ the similarity kernel for samples.}
+#'  \item{\code{sample.col}}{ the color for plotting each sample point.}
+#'  \item{\code{sample.pch}}{ the shape for plotting each sample point.}
+#'  \item{\code{otu.names}}{ the taxonomic name of each OTU.}
+#
+#'
+#' }
+#' @references Satten GA, Tyx RE, Rivera AJ, Stanfill S. 2017. Restoring the duality between principal components of a distance matrix and linear combina- tions of predictors, with application to studies of the microbiome. PLoS
+#' ONE 12(1)
+#' @examples data(tobacco_clr)
